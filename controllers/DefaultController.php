fea19f082f3bf051b355228aafc0802b83be3f9a
Alexandr <alex@webgoal.ru>
2020-11-26 15:46:42 +0300

    Update

---
+++ controllers/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace alex290\widgetContent\controllers;
+
+use yii\web\Controller;
+
+class DefaultController extends Controller
+{
+    public function actionIndex()
+    {
+        return TRUE;
+    }
+    
+}
