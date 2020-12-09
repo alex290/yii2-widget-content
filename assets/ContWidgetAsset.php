4d5bea852ca55e74f776ac65b2ebe720743ea6f1
Alexandr <alexmar12@yandex.ru>
2020-12-09 16:39:24 +0300

    Update ContWidgetAsset.php

--- assets/ContWidgetAsset.php
+++ assets/ContWidgetAsset.php
@@ -31,19 +31,6 @@ class ContWidgetAsset extends AssetBundle
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
     ];
-    public function init()
-    {
-        parent::init();
-        // resetting BootstrapAsset to not load own css files
-        \Yii::$app->assetManager->bundles['yii\\bootstrap\\BootstrapAsset'] = [
-            'css' => [],
-            'js' => []
-        ];
-        \Yii::$app->assetManager->bundles['yii\\bootstrap\\BootstrapPluginAsset'] = [
-            'css' => [],
-            'js' => []
-        ];
-    }
+
 }
