ae91234e3cbc72d3381a37c59b85a1f15a7ed1f1
alex <alex@webgoal.ru>
2021-04-07 13:01:04 +0300

    Update

--- Rules.php
+++ Rules.php
@@ -12,6 +12,10 @@ class Rules
             foreach ($field as $key => $value) {
                 if ($value[0] == 'image') {
                     $model->addRule($key, 'file', ['extensions' => 'png, jpg']);
+                } elseif ($value[0] == 'file') {
+                    $model->addRule($key, 'file');
+                } elseif ($value[0] == 'select') {
+                    $model->addRule($key, 'string', ['max' => 255]);
                 } else {
                     if (array_key_exists('max', $value)) $model->addRule($key, $value[0], ['max' => $value['max']]);
                     else $model->addRule($key, $value[0]);
@@ -26,9 +30,17 @@ class Rules
     {
         if (!empty($field)) {
             foreach ($data as $key => $value) {
-                if (array_key_exists('max', $field[$key])) $model->addRule($key, $field[$key][0], ['max' => $field[$key]['max']]);
-                else $model->addRule($key, $field[$key][0]);
-                $model->$key = $value;
+                if ($field[$key][0] == 'file') {
+                    $model->addRule($key, 'file');
+                } elseif ($field[$key][0] == 'select') {
+                    $model->addRule($key, 'string', ['max' => 255]);
+                    $model->$key = $value;
+                } else {
+                    if (array_key_exists('max', $field[$key])) $model->addRule($key, $field[$key][0], ['max' => $field[$key]['max']]);
+                    else $model->addRule($key, $field[$key][0]);
+                    $model->$key = $value;
+                }
+
             }
         }
 
